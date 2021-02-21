6e2742e05ce4c088ac1cfbb67f7a52cfc2351100
Maximilian <max.hvesser@finn.no>
2021-02-21 14:39:19 +0100

    Renamed main -> home, added UI package

--- app/src/main/java/no/mhl/wikitopics/ui/home/HomeActivity.kt
+++ app/src/main/java/no/mhl/wikitopics/ui/home/HomeActivity.kt
@@ -1,11 +1,14 @@
-package no.mhl.wikitopics
+package no.mhl.wikitopics.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import no.mhl.wikitopics.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
+
 }

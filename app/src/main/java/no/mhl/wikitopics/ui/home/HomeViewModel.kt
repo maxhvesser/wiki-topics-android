77cb4d42e66875b73337b1f5ff0670c64d37587c
Maximilian <max.hvesser@finn.no>
2021-02-21 15:27:44 +0100

    Consumed repo function in ViewModel

--- app/src/main/java/no/mhl/wikitopics/ui/home/HomeViewModel.kt
+++ app/src/main/java/no/mhl/wikitopics/ui/home/HomeViewModel.kt
@@ -1,7 +1,10 @@
 package no.mhl.wikitopics.ui.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import no.mhl.wikitopics.api.common.Resource
+import no.mhl.wikitopics.model.WikiResponse
 import no.mhl.wikitopics.repository.HomeRepository
 import javax.inject.Inject
 
@@ -10,4 +13,11 @@ class HomeViewModel
 @Inject
 constructor(
     private val homeRepository: HomeRepository
-): ViewModel()
+): ViewModel() {
+
+    // region Remote Interaction
+    fun pageForTopic(topic: String): LiveData<Resource<WikiResponse>> = homeRepository
+            .getWikiPageForTopicFromApi(topic)
+    // endregion
+
+}

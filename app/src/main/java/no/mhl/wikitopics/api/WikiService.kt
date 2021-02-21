8af745e3f67030c0ede92bad80d284eaffa49d2a
Maximilian <max.hvesser@finn.no>
2021-02-21 14:48:30 +0100

    Added service call with endpoints

---
+++ app/src/main/java/no/mhl/wikitopics/api/WikiService.kt
@@ -0,0 +1,24 @@
+package no.mhl.wikitopics.api
+
+import no.mhl.wikitopics.api.common.Constants.ENDPOINT_API
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WikiService {
+
+    /**
+     * Remote call to get a Wiki page response object from a given topic
+     * @param page The topic for which page we want to be returned
+     * @return The response [Any] wrapped in a [Call]
+     */
+    @GET(ENDPOINT_API)
+    fun getWikiPageForTopic(
+        @Query("action") action: String = "parse",
+        @Query("section") section: Int = 0,
+        @Query("prop") prop: String = "text",
+        @Query("format") format: String = "json",
+        @Query("page") page: String
+    ): Call<Any>
+
+}
---
+++ app/src/main/java/no/mhl/wikitopics/api/common/Constants.kt
@@ -0,0 +1,10 @@
+package no.mhl.wikitopics.api.common
+
+object Constants {
+
+    // region API Endpoints
+    const val BASE_URL = "https://en.wikipedia.org/w/"
+    const val ENDPOINT_API = "api.php"
+    // endregion
+
+}

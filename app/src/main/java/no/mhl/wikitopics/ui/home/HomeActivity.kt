7dec5b83bacd59746a298e8e6688bebef662f079
Maximilian <max.hvesser@finn.no>
2021-02-21 14:42:12 +0100

    Added viewmodel, setup bindings

--- app/src/main/java/no/mhl/wikitopics/ui/home/HomeActivity.kt
+++ app/src/main/java/no/mhl/wikitopics/ui/home/HomeActivity.kt
@@ -2,13 +2,29 @@ package no.mhl.wikitopics.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import dagger.hilt.android.AndroidEntryPoint
 import no.mhl.wikitopics.R
+import no.mhl.wikitopics.databinding.ActivityHomeBinding
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
+    // region Bindings
+    private lateinit var model: HomeViewModel
+    private lateinit var binding: ActivityHomeBinding
+    // endregion
+
+    // region Initialization
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        model = ViewModelProvider(this).get()
+
+        setContentView(binding.root)
     }
+    // endregion
 
 }
